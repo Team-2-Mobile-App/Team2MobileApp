b03aff6ebfcdac846fc2efd150b6639e08e70122
loampo <loampo1@gmail.com>
2023-11-23 01:27:52 +0100

    Movement and TMP

---
+++ Assets/Scenes/Michele/CameraMoveTest.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveTest : MonoBehaviour
+{
+    private Vector3 _touchStartPosition;
+    private Vector3 _touchEndPosition;
+    private Camera _camera;
+    private RaycastHit _hit;
+    public float raycastDistance = 10f;
+    public float touchSensibility = 1f;
+    private Portal CheckPortal;
+    public LayerMask portalLayer;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount <= 0)
+        {
+            return;
+        }
+
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            _touchStartPosition = touch.position;
+            Ray ray = _camera.ScreenPointToRay(_touchStartPosition);
+            Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.red, 1.0f);
+
+            if (Physics.Raycast(ray, out _hit, raycastDistance, portalLayer))
+            {
+                if (_hit.collider.TryGetComponent(out Portal portal))
+                {
+                    Debug.Log("Entrato");
+                    CheckPortal = portal;
+                }
+                else CheckPortal = null;
+            }
+        }
+
+        if (touch.phase == TouchPhase.Ended)
+        {
+            _touchEndPosition = touch.position;
+            Ray ray = _camera.ScreenPointToRay(_touchEndPosition);
+            Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.blue, 1.0f);
+
+            if (CheckPortal != null && Vector3.Distance(_touchStartPosition, _touchEndPosition) <= touchSensibility)
+            {
+                CheckPortal.MoveToNewPosition(this.transform);
+                CheckPortal = null;
+            } 
+            else CheckPortal = null;
+        }
+    }
+}
